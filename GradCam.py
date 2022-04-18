244383584e084c502cec63e8c01062e485a62385
Dhruv Kapur <37783178+dkapur17@users.noreply.github.com>
2022-04-19 01:54:29 +0530

    Add files via upload

---
+++ GradCam.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib as mpl
+import numpy as np
+
+
+from torchvision import transforms
+from PIL import Image
+from typing import Union
+
+
+class GradCam(nn.Module):
+
+    def __init__(self, model: nn.Module, layer: nn.Module, device) -> None:
+        super(GradCam, self).__init__()
+        self.device = device
+        self.model = model
+        self.layer = layer
+
+        self.gradients = None
+        self.tensor_hook = None
+        self.activation_maps = None
+
+        def forward_hook(model: nn.Module, inp: torch.Tensor, out: torch.Tensor) -> None:
+            self.activation_maps = out
+            self.tensor_hook = self.activation_maps.register_hook(
+                self.backward_hook)
+
+        self.layer_hook = self.layer.register_forward_hook(forward_hook)
+
+        self.image_transforms = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                0.229, 0.224, 0.255]),
+            transforms.Lambda(lambda t: t.unsqueeze(0))
+        ])
+
+    def backward_hook(self, grad: torch.Tensor) -> None:
+        self.gradients = grad
+
+    def _compute_cam(self, X: torch.Tensor, y) -> torch.Tensor:
+        out = self.model(X)
+        activation_maps = self.activation_maps.detach().cpu()
+        if y is None:
+            out[:, out.argmax()].backward()
+        else:
+            assert y.shape == torch.Size([1])
+            out[:, y].backward()
+
+        scores = torch.mean(self.gradients.detach().cpu(),
+                            dim=[0, 2, 3]).detach().cpu()
+
+        for i in range(activation_maps.shape[1]):
+            activation_maps[:, i, :, :] *= scores[i]
+
+        heatmap = torch.mean(activation_maps, dim=1).squeeze()
+        heatmap = F.relu(heatmap)
+        return heatmap / torch.max(heatmap)
+
+    def forward(self, image: Image, label: Union[torch.Tensor, None] = None, superimpose: bool = True) -> Image:
+        inp = self.image_transforms(image).to(self.device)
+        cam = self._compute_cam(inp, label)
+        inv_transforms = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(image.size[::-1])
+        ])
+        cam = inv_transforms(cam)
+
+        if superimpose:
+            cmap = mpl.cm.get_cmap('jet', 256)
+            cam = np.array(cmap(cam))
+            cam = Image.fromarray((cam * 255).astype(np.uint8))
+            cam.putalpha(128)
+            image.paste(cam, (0, 0), cam)
+            return image
+        else:
+            return cam
